752c21580b021106d7116eb04f81cfb5d454aa78
Luís Ângelo Rodrigues Júnior <luisangelorjr@gmail.com>
2017-04-06 23:31:08 -0300

    Configurado para Chrome abrir o link

--- k4j.bat
+++ k4j.bat
@@ -22,13 +22,13 @@ md c:\larjr\exe\WinWGet
 
 rem Download dos arquivos
 rem WinWGetPortable
-start "https://downloads.sourceforge.net/project/portableapps/WinWGet%20Portable/WinWGetPortable_0.20.0.3_English.paf.exe?r=http%3A%2F%2Fportableapps.com%2Fapps%2Finternet%2Fwinwget_portable&ts=1491526276&use_mirror=ufpr"
+start chrome.exe "https://downloads.sourceforge.net/project/portableapps/WinWGet%20Portable/WinWGetPortable_0.20.0.3_English.paf.exe?r=http%3A%2F%2Fportableapps.com%2Fapps%2Finternet%2Fwinwget_portable&ts=1491526276&use_mirror=ufpr"
 
 rem JkDefragPortable
-start https://downloads.sourceforge.net/project/portableapps/JkDefrag%20Portable/JkDefragPortable_3.36_Rev_2.paf.exe?r=http%3A%2F%2Fportableapps.com%2Fapps%2Futilities%2Fjkdefrag_portable&ts=1491526221&use_mirror=ufpr
+start chrome.exe "https://downloads.sourceforge.net/project/portableapps/JkDefrag%20Portable/JkDefragPortable_3.36_Rev_2.paf.exe?r=http%3A%2F%2Fportableapps.com%2Fapps%2Futilities%2Fjkdefrag_portable&ts=1491526221&use_mirror=ufpr"
 
 rem CCleaner Portable 3.27 (atual 5.28)
 rem missing
 
 rem CPU-Z Portable 
-start http://portableapps.com/redirect/?a=CPU-ZPortable&t=http%3A%2F%2Fdownloads.portableapps.com%2Fportableapps%2Fcpu-zportable%2FCPU-ZPortable_1.78.3_English.paf.exe
+start chrome.exe "http://portableapps.com/redirect/?a=CPU-ZPortable&t=http%3A%2F%2Fdownloads.portableapps.com%2Fportableapps%2Fcpu-zportable%2FCPU-ZPortable_1.78.3_English.paf.exe"
