958e18053588a0e8a5d1dc0191646cddf4021d5f
Luís Ângelo Rodrigues Júnior <luisangelorjr@gmail.com>
2017-04-06 23:22:11 -0300

    Link de download das ferramentas

---
+++ k4j.bat
@@ -0,0 +1,11 @@
+WinWGetPortable
+https://downloads.sourceforge.net/project/portableapps/WinWGet%20Portable/WinWGetPortable_0.20.0.3_English.paf.exe?r=http%3A%2F%2Fportableapps.com%2Fapps%2Finternet%2Fwinwget_portable&ts=1491526276&use_mirror=ufpr
+
+JkDefragPortable
+https://downloads.sourceforge.net/project/portableapps/JkDefrag%20Portable/JkDefragPortable_3.36_Rev_2.paf.exe?r=http%3A%2F%2Fportableapps.com%2Fapps%2Futilities%2Fjkdefrag_portable&ts=1491526221&use_mirror=ufpr
+
+CCleaner Portable 3.27 (atual 5.28)
+
+
+CPU-Z Portable 
+http://portableapps.com/redirect/?a=CPU-ZPortable&t=http%3A%2F%2Fdownloads.portableapps.com%2Fportableapps%2Fcpu-zportable%2FCPU-ZPortable_1.78.3_English.paf.exe
