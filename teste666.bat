d9cdd943d71036f37d5e890dca898399c3002914
Luís Ângelo Rodrigues Júnior <luisangelorjr@gmail.com>
2017-04-07 01:55:31 -0300

    Teste com IF e ELSE

---
+++ teste666.bat
@@ -0,0 +1,7 @@
+::teste sobre if e else
+
+@echo off
+if exist "C:\Users\tsystems418\Downloads\jk.7z" (move "c:\Users\tsystems418\Downloads\jk.7z" "c:\larjr\exe\CPUz\") else echo Hohohohoh
+::usar depois set /p
+pause
+pause
